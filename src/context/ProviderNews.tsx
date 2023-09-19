4f3a544e00b195a6fddc086d23efef79c1bed6fb
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-09-19 20:17:52 -0300

    dateFormat function problems fixed

--- src/context/ProviderNews.tsx
+++ src/context/ProviderNews.tsx
@@ -13,37 +13,38 @@ function ProviderNews({ children }: ProviderNewsProps) {
   const [favorites, setFavorites] = useState<NewsProps[]>([]);
 
   const dateFormat = (date: string) => {
-    const dateParts = date?.split(/[\s/:-]+/);
-    const newsDate = dateParts ? new Date(
+    const dateParts = date.split(/[\s/:-]+/); 
+    const newsDate = new Date(
       Number(dateParts[2]), 
       Number(dateParts[1]) - 1, 
       Number(dateParts[0]), 
-      Number(dateParts[3]),  
+      Number(dateParts[3]), 
       Number(dateParts[4]), 
-      Number(dateParts[5])   
-    ): new Date();
+      Number(dateParts[5]) 
+    );
+  
     const currentDate = new Date();
-    const timeDifference = Math.round((currentDate.getTime() - newsDate.getTime()) / (1000 * 60 * 60 * 24));
+    const timeDifference = Math.round((currentDate.getTime() - newsDate.getTime()) / 1000); 
   
     const formatTimeAgo = (timeAgo: number) => {
-      const seconds = timeAgo * 24 * 60 * 60;
-      const minutes = timeAgo * 24 * 60;
-      const hours = timeAgo * 24;
-      const days = timeAgo;
-  
-      if (seconds < 60) {
-        return `${seconds} segundos atrás`;
-      }else if (minutes < 60) {
+      if (timeAgo < 60) {
+        return `${timeAgo} segundos atrás`;
+      } else if (timeAgo < 3600) { 
+        const minutes = Math.floor(timeAgo / 60);
         return `${minutes} minutos atrás`;
-      }else if (hours < 24) {
+      } else if (timeAgo < 86400) { 
+        const hours = Math.floor(timeAgo / 3600);
         return `${hours} horas atrás`;
-      }else {
+      } else {
+        const days = Math.floor(timeAgo / 86400);
         return `${days} dias atrás`;
       }
     };
   
     return formatTimeAgo(timeDifference);
-  }
+  };
+  
+  
   
   useEffect(() => {
     const fetchNews = async () => {
